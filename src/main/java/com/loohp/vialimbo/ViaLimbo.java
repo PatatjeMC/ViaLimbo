6660c412e902b18e8e212336a5b4f54733e0d39a
LOOHP <jamesloohp@gmail.com>
2025-06-21 13:56:05 +0100

    Minecraft 1.21.6

--- src/main/java/com/loohp/vialimbo/ViaLimbo.java
+++ src/main/java/com/loohp/vialimbo/ViaLimbo.java
@@ -1,3 +1,23 @@
+/*
+ * This file is part of ViaLimbo.
+ *
+ * Copyright (C) 2020 - 2025. LoohpJames <jamesloohp@gmail.com>
+ * Copyright (C) 2020 - 2025. Contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.loohp.vialimbo;
 
 import com.google.common.cache.Cache;
@@ -17,7 +37,6 @@ import net.lenni0451.classtransform.mixinstranslator.MixinsTranslator;
 import net.lenni0451.classtransform.utils.tree.IClassProvider;
 import net.lenni0451.optconfig.ConfigLoader;
 import net.lenni0451.optconfig.provider.ConfigProvider;
-import net.lenni0451.reflect.Agents;
 import net.raphimc.viaproxy.ViaProxy;
 import net.raphimc.viaproxy.plugins.events.Proxy2ServerChannelInitializeEvent;
 import net.raphimc.viaproxy.protocoltranslator.ProtocolTranslator;
@@ -102,7 +121,6 @@ public class ViaLimbo extends LimboPlugin implements Listener {
             TransformerManager transformerManager = new TransformerManager(classProvider);
             transformerManager.addTransformerPreprocessor(new MixinsTranslator());
             transformerManager.addTransformer("net.raphimc.viaproxy.injection.mixins.**");
-            transformerManager.hookInstrumentation(Agents.getInstrumentation());
 
             ConfigLoader<ViaProxyConfig> configLoader = new ConfigLoader<>(ViaProxyConfig.class);
             configLoader.getConfigOptions().setResetInvalidOptions(true).setRewriteConfig(true).setCommentSpacing(1);
